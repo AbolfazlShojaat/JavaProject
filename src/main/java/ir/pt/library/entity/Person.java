87050baaa97327d5d2909f30e7b490cf085d8733
a@gmail.com <a@gmail.com>
2022-11-10 09:49:13 +0330

    change package name

--- src/main/java/ir/pt/library/entity/Person.java
+++ src/main/java/ir/pt/library/entity/Person.java
@@ -1,4 +1,48 @@
 package ir.pt.library.entity;
 
+
 public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int nationalCode;
+
+    public Person(int id, String firstName, String lastName, int nationalCode) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationalCode = nationalCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(int nationalCode) {
+        this.nationalCode = nationalCode;
+    }
 }
